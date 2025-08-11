2e9a44afdc1bf85cb8406e71c43448610f47c3ad
Amir Mahdi Barati <amir.nytrix@gmail.com>
2025-08-11 19:30:48 +0330

    Update crypto.ino

--- crypto.ino
+++ crypto.ino
@@ -11,13 +11,13 @@ WebServer server(80);
 #define AES_KEY_SIZE 16  // 128-bit AES
 #define AES_BLOCK_SIZE 16
 
-// IV ثابت (برای نمونه) - در عمل بهتر است IV تصادفی و در ابتدای پیام ارسال شود
+
 const uint8_t iv[AES_BLOCK_SIZE] = {
   0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
   0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f
 };
 
-// -- HTML صفحه (همان UI قبلی با تغییرات جزیی برای انتخاب الگوریتم و ورودی کلید)
+
 const char index_html[] PROGMEM = R"rawliteral(
 <!doctype html>
 <html>
